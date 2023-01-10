e008e4a1d7ab82880cd922849e15889c2b01c7e3
robelzxc <robeljohnsongcog9@gmail.com>
2023-01-11 01:57:23 +0800

    Changing bottomNavigationBar into ElevatedButtons and adjusted the design

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,13 +22,21 @@ class _HomePageState extends State<HomePage> {
       final image = await ImagePicker().pickImage(source: source);
       if (image == null) return;
 
-      final imgTemporary = File(image.path);
+      final imgPermanent = await saveImgPermanently(image.path);
       setState(() {
-        this.image = imgTemporary;
+        this.image = imgPermanent;
       });
     } on PlatformException catch (e) {
       print("Failed to pick image: $e");
     }
+  }
+
+  Future<File> saveImgPermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final image = File('${directory.path}/$name');
+
+    return File(imagePath).copy(image.path);
   }
 
   @override
@@ -35,91 +45,104 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Activity 5"),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54.withOpacity(0.5),
-                  spreadRadius: 10,
-                  blurRadius: 10,
-                )
-              ],
-              border: Border.all(width: 5, color: Colors.white70),
-            ),
-            child: image != null
-                ? CircleAvatar(
-                    radius: 150,
-                    child: Image.file(
+      body: Container(
+        margin: const EdgeInsets.all(50),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 8, color: Colors.white70),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.white.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ]),
+              child: image != null
+                  ? Image.file(
                       image!,
                       width: 300,
                       height: 300,
                       fit: BoxFit.cover,
-                    ),
-                  ):
-                const Image(
+                    )
+                  : const Image(
                       image: AssetImage('assets/chishiya.jpg'),
                       fit: BoxFit.cover,
                       width: 300,
                       height: 300,
                     ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 50,
                 ),
-          Container(
-            padding: const EdgeInsets.only(left: 50, right: 50),
-            child: const Text(
-              "Robel qt",
-              style: TextStyle(
-                fontSize: 45,
-                color: Colors.yellow,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          const Text(
-            ": Grabi jud ka gwapo",
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              color: Colors.white54,
-            ),
-          ),
-          const Text(
-            ": Kaayo Jud",
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              color: Colors.white54,
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Gallery'),
-        ],
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        onTap: (int index) async {
-          if(index == _selectedIndex) {
-            PermissionStatus cameraStatus = await Permission.camera.request();
-            if (cameraStatus == PermissionStatus.granted) {
-                pickImage(ImageSource.camera);
-            } else if (cameraStatus == PermissionStatus.denied) {
-              return;
-            }
-          }else{
-            PermissionStatus galleryStatus = await Permission.storage.request();
-            if (galleryStatus == PermissionStatus.granted) {
-                pickImage(ImageSource.gallery);
-            } else if (galleryStatus == PermissionStatus.denied) {
-              return;
-            }
-          }
-        },
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      elevation: 10, shadowColor: Colors.white,
+                    ),
+                    onPressed: () async {
+                      PermissionStatus cameraStatus =
+                          await Permission.camera.request();
+                      if (cameraStatus == PermissionStatus.granted) {
+                        pickImage(ImageSource.camera);
+                      } else if (cameraStatus == PermissionStatus.denied) {
+                        return;
+                      }
+                    },
+                    child: Column(
+                      children: const [
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Icon(Icons.camera_alt),
+                        Text('Camera'),
+                        SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    )),
+                const SizedBox(
+                  width: 25,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    elevation: 10, shadowColor: Colors.white,
+                  ),
+                  onPressed: () async {
+                    PermissionStatus galleryStatus =
+                        await Permission.storage.request();
+                    if (galleryStatus == PermissionStatus.granted) {
+                      pickImage(ImageSource.gallery);
+                    } else if (galleryStatus == PermissionStatus.denied) {
+                      return;
+                    }
+                  },
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Icon(Icons.image),
+                      Text('Gallery'),
+                      SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
